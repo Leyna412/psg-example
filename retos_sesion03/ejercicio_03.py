28d3683d49ad38890bd1b8f5aef001ce052c9ce8
Leyna Yanarico <leynaa123@gmail.com>
2025-05-10 14:46:48 -0400

    ¿Como lavar los platos?

---
+++ retos_sesion03/ejercicio_03.py
@@ -0,0 +1,15 @@
+print("¿COMO LAVAR LOS PLATOS")
+print("1. Preparación:" 
+      "- Retirar los restos de comida de los platos con una espátula o servilleta."
+      "- Despejar el fregadero de cualquier objeto y asegurarte de que esté limpio")
+print("2. Lavar: " \
+"- Llenar el fregadero con agua caliente y jabón para lavavajillas."
+"- Sumergir los platos en el agua jabonosa."
+"- Fregar los platos con una esponja o paño, comenzando por los menos sucios y terminando por los más sucios."
+"- Para platos con comida adherida, se puede usar una escobilla o cepillo de cocina.")
+print("3. Enjuagar:" \
+"- Enjuagar los platos con agua caliente, asegurándose de eliminar todo el jabón." \
+"- Para platos con grasa, se puede usar un paño limpio para secar y evitar la formación de manchas.")
+print("4. Secar:" \
+"- Colocar los platos limpios sobre un escurridor o toalla para que se sequen al aire." \
+"- Para vasos y copas, se puede usar un paño limpio para secarlos y evitar manchas.")
