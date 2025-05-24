75e82a411d432562473d557b9c07c30c6334c72d
Leyna Yanarico <leynaa123@gmail.com>
2025-05-24 02:15:27 -0400

    Precio total

---
+++ retos_sesion05/ejercicio_01.py
@@ -0,0 +1,12 @@
+print("Calcule el total total a pagar si se compran 12 productos iguales donde cada uno cuesta 3.75 USD.")
+print ("RESOLUCIÓN")
+precio_unitario = 3.75
+cantidad = 12 
+print("precio_unitario = 3.75")
+print("cantidad = 12 ")
+# Calculo del Total
+print ("Calculo del Total")
+print( "total = precio_unitario * cantidad")
+total = precio_unitario * cantidad
+#Mostrar el resultado
+print("El Total a pagar es: USD", total)
