02e31d21b5c160f4744599e4c102b17507edcda2
Leyna Yanarico <leynaa123@gmail.com>
2025-05-24 02:30:11 -0400

    Fahrenheit a Celsius

---
+++ retos_sesion05/ejercicio_02.py
@@ -0,0 +1,23 @@
+print("convierta las siguientes temperaturas de grados Fahrenheit a grados Celsius:")
+print("25 ºF, 300 ºF, 76 ºF")
+print ("RESOLUCIÓN")
+temp1 = 25
+temp2 = 300
+temp3 = 76
+print("temp1 = 25")
+print("temp2 = 300")
+print("temp3 = 76")
+# Conversión a Celsius
+print("# Conversión a Celsius usando operaciones aritmeticas")
+print("# Para temp1")
+celsius1 = (temp1 - 32) * 5 / 9 
+print("celsius1 =", celsius1)
+print("25 ºF =", round(celsius1, 2), "ºC")
+print("# Para temp2")
+celsius2 = (temp2 - 32) * 5 / 9 
+print("celsius2 =", celsius2)
+print("300 ºF =", round(celsius2, 2), "ºC")
+print("# Para temp3")
+celsius3 = (temp3 - 32) * 5 / 9 
+print("celsius3 =", celsius3)
+print("25 ºF =", round(celsius3, 2), "ºC")
