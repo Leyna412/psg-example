8e088cb6c9f27340cd54fe1d74076834119b9a42
Leyna Yanarico <leynaa123@gmail.com>
2025-05-24 03:08:20 -0400

    Clase sesion 05

---
+++ Practicas/sesion05.py
@@ -0,0 +1,67 @@
+# Numeros Enteros
+# Valor 10 Entero
+print (10)
+print ( type (10) )
+
+# Variable 100 Entero
+variable = 100
+print (variable)
+print ( type (variable) )
+
+# Valor 10 en base decimal
+print ("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("Base binaria")
+print (0b1010)
+# Valor 10 en octal
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal
+print ("Base hexadecimal")
+print (0xa)
+
+# NUMERO FLOTANTE
+# Valor 0.5 Flotante
+print (0.5)
+print ( type (0.5) )
+
+# Variable 0.100546 Flotante
+variable_4 = 0.100546
+print (variable_4)
+print ( type (variable_4) )
+
+# Valor 2.0e-3 Flotante
+variable_6 = 2.0e-3
+print(variable_6)
+print ( type (variable_6) )
+
+# OPERACIONES COMPLEJAS
+print("Un contador tiene 300 minutos y se le suman 3600 segundos, ¿Cuántas horas en total son?")
+minutos = 300
+segundos = 3600
+min_adic = segundos / 60
+t_min = segundos + min_adic
+horas = t_min / 60
+print("Total de horas:", horas,"horas")
+
+# OPERACIONES DE COMPARACIÓN
+print ("Operadores de comparación")
+comparar = 10
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+
+#Flotante
+print ("Operadores de comparación con int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
