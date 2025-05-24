6b157771cd876e73ac0e392d0a4f328b342fb8ce
Leyna Yanarico <leynaa123@gmail.com>
2025-05-24 02:55:05 -0400

    Volumen de Cubo

---
+++ retos_sesion05/ejercicio_04.py
@@ -0,0 +1,13 @@
+print("Calcular el volumen de un cubo donde uno de sus lados mide 15 metros, utilizando la fórmula general del volumen de un cubo.")
+print ("RESOLUCIÓN")
+# Longitud del lado del cubo en metros
+print("# Longitud del lado del cubo en metros")
+lado = 15
+print("lado = 15")
+# Calculo del Volumen del cubo
+print("# Calculo del Volumen del cubo")
+volumen = lado ** 3
+print("volumen = lado ** 3")
+# Mostrar los resultados
+print("RESULTADOS")
+print("El volumen del cubo es:", volumen, "metros cubicos")
