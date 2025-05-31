243c2cf3db9c440638e39e2e3be2d21e24b9ebc4
Leyna Yanarico <leynaa123@gmail.com>
2025-05-31 15:20:16 -0400

    XNOT

---
+++ retos_sesion06/ejercicio_02.py
@@ -0,0 +1,8 @@
+def xnor(a, b):
+    return a == b
+
+# Ejemplos
+print(xnor(True, True))    # True
+print(xnor(False, False))  # True
+print(xnor(True, False))   # False
+print(xnor(False, True))   # False
