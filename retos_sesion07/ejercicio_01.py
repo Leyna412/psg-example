76e58c3323f9d5290bd05dc1fc9563687761d8a6
Leyna Yanarico <leynaa123@gmail.com>
2025-06-07 14:18:41 -0400

    Resta de números

---
+++ retos_sesion07/ejercicio_01.py
@@ -0,0 +1,7 @@
+poema = """Y el cuervo, nunca iniciando vuelo, aún está, aún está
+en el pálido busto de Palas justo encima de la puerta de mi cuarto;
+y sus ojos tienen la apariencia de un demonio que está soñando,
+y la luz de la lámpara que lo baña, lanza su sombra en el suelo;
+y mi alma, fuera de esa sombra que yace flotando en el suelo,
+no podrá liberarse... ¡nunca más!"""
+print(poema)
