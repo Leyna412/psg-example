f0625fc7ec224d13241fa26eef9da0e7dde9c6c8
Leyna Yanarico <leynaa123@gmail.com>
2025-06-14 06:25:29 -0400

    tuplas y coordenadas

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,13 @@
+# Tuplas de coordenadas
+punto1 = (-20, -20)
+punto2 = (40, 20)
+
+# Cálculo del punto medio
+x_medio = (punto1[0] + punto2[0]) / 2
+y_medio = (punto1[1] + punto2[1]) / 2
+
+# Almacenar en una tupla
+punto_medio = (x_medio, y_medio)
+
+# Imprimir resultado
+print("El punto medio es:", punto_medio)
