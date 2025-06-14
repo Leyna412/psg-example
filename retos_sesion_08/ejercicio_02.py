4fadd7dd9797699270c6bacda0617716e76728b2
Leyna Yanarico <leynaa123@gmail.com>
2025-06-14 06:14:16 -0400

    Tupla

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,17 @@
+# Crear la tupla
+mi_tupla = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+# Imprimir el primer elemento
+print(mi_tupla[0])  
+
+# Imprimir el último elemento
+print(mi_tupla[-1]) 
+
+# Imprimir un slice del índice 3 al 5
+print(mi_tupla[3:6]) 
+
+# Imprimir un slice del 5 al 9 con pasos de 3
+print(mi_tupla[5:10:3]) 
+
+# Imprimir un slice del 9 al 0 con pasos de -2
+print(mi_tupla[9::-2])
