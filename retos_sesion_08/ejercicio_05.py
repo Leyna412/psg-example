c197aaa74cb7e006f1dd8c10ed179f2c2225c338
Leyna Yanarico <leynaa123@gmail.com>
2025-06-14 06:22:03 -0400

    guia telefonica

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,10 @@
+# Crear la tupla anidada con los datos de la guía telefónica
+guia_telefonica = (
+    ("Jhon", "591123456", "Villa fátima"),
+    ("Jane", "591546324", "Obrajes"),
+    ("Dogg", "591222421", "Miraflores"),
+    ("Kity", "591753159", "Achumani")
+)
+
+# Imprimir la tupla anidada
+print(guia_telefonica)
