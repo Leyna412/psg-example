ab07965465867e9b8e4234abc37b018b8532b287
Leyna Yanarico <leynaa123@gmail.com>
2025-07-19 22:37:11 -0400

    Autos

---
+++ retos_sesion_12/ejercicio_03.py
@@ -0,0 +1,11 @@
+# Colección de autos de Jhon
+autos_jhon = {'Ferrari', 'Lamborghini', 'Porsche', 'Bugatti', 'McLaren'}
+
+# Colección de autos de Jess
+autos_jess = {'Ferrari', 'Lamborghini', 'Tesla', 'Ford', 'Chevrolet'}
+
+# Autos en común (intersección de conjuntos)
+autos_comunes = autos_jhon & autos_jess  
+
+# Mostrar resultados
+print("Autos que tienen en común Jhon y Jess:", autos_comunes)
