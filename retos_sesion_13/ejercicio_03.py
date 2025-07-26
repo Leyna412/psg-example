e5fd2da98e9566b48033bec27e6ac1024db6d137
Leyna Yanarico <leynaa123@gmail.com>
2025-07-26 12:09:37 -0400

    estudiantes aprobados

---
+++ retos_sesion_13/ejercicio_03.py
@@ -0,0 +1,7 @@
+estudiantes = [('Miguel', 51), ('Daniel', 80), ('Virginia', 90), ('Franco', 40), ('Flabio', 30)]
+
+print("Felicitaciones a los siguientes estudiantes por aprobar el curso:")
+
+for nombre, nota in estudiantes:
+    if nota >= 51:
+        print(f"¡Felicidades {nombre}! Has aprobado con {nota} puntos.")
